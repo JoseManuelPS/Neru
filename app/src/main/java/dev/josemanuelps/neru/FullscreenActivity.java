2020b9e80c532c522070fe55e49e3a10b59cffde
José Manuel Plana Santos <dev.josemanuelps@gmail.com>
2020-04-29 09:14:27 +0200

    Playing ambience sounds implemented.

--- app/src/main/java/dev/josemanuelps/neru/FullscreenActivity.java
+++ app/src/main/java/dev/josemanuelps/neru/FullscreenActivity.java
@@ -2,11 +2,18 @@ package dev.josemanuelps.neru;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class FullscreenActivity extends AppCompatActivity {
@@ -14,11 +21,15 @@ public class FullscreenActivity extends AppCompatActivity {
     private TextView screen;
     private TextView screen_end;
     private TextView screen_top;
+    private ImageButton button_sound;
     private Thread thread;
     private Vibrator vibrator;
+    private MediaPlayer mediaPlayer;
+    private Resources resources;
 
-    private boolean running=false;
-    private boolean stop_flag =false;
+    private boolean running = false;
+    private boolean stop_flag = false;
+    private boolean playing = true;
     private int count = 0;
 
     @Override
@@ -34,6 +45,7 @@ public class FullscreenActivity extends AppCompatActivity {
 
         screen_end = findViewById(R.id.screen_end);
         screen_top = findViewById(R.id.screen_top);
+        button_sound = findViewById(R.id.button_sound);
 
         screen = findViewById(R.id.screen);
         screen.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
@@ -59,12 +71,37 @@ public class FullscreenActivity extends AppCompatActivity {
         screen.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                thread.interrupt();
+                try{
+                    thread.interrupt();
+                }catch (Exception e){
+                    // Do nothing
+                }
                 running = false;
                 screen.setText(getString(R.string.screen));
                 screen_end.setVisibility(View.INVISIBLE);
                 screen_top.setVisibility(View.INVISIBLE);
                 return false;
+            }
+        });
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.universe);
+        mediaPlayer.start();
+        resources = getResources();
+
+        button_sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (playing){
+                    mediaPlayer.pause();
+                    Drawable drawable_off = ResourcesCompat.getDrawable(resources, android.R.drawable.ic_lock_silent_mode, null);
+                    button_sound.setImageDrawable(drawable_off);
+                    playing = false;
+                } else {
+                    mediaPlayer.start();
+                    Drawable drawable_on = ResourcesCompat.getDrawable(resources, android.R.drawable.ic_lock_silent_mode_off, null);
+                    button_sound.setImageDrawable(drawable_on);
+                    playing = true;
+                }
             }
         });
 
@@ -131,5 +168,11 @@ public class FullscreenActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) mediaPlayer.release();
     }
 }
